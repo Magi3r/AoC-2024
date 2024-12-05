15ebd5dee74fa68835acffe6b6ae57ad7639a7f9
Magi3r <jvd@outlook.de>
2024-12-05 19:56:03 +0100

    publish day 5 part 2

--- day05/src/day05.rb
+++ day05/src/day05.rb
@@ -10,7 +10,7 @@ def prepare(path)
 	rules.default=[]
 	rule_lines.split.each do |line|
 		before, after = line.split("|")
-		puts before +" before "+after
+		#puts before +" before "+after
 		if rules[before.to_i]==[] then
 			rules[before.to_i]=[after.to_i]
 		else
@@ -55,8 +55,42 @@ def part1 rules, updates
 	return result
 end
 
-def part2
+def is_valid rules, value, list
+	list.each do |t_u|
+		if rules[value].include? t_u then
+			#puts "BREAK: " + value.to_s + " must be before "+ t_u.to_s
+			#puts update_batch.inspect
+			return false
+		end
+	end
+	return true
+end
 
+def part2 rules, updates
+	result = 0
+
+	updates.each do |update_batch|
+		temp_update_batch = []
+		incorrect = false
+		update_batch.each do |u|
+			temp_move_backward = []
+
+			while not (is_valid(rules, u, temp_update_batch)) do
+				incorrect = true
+				# puts "not valid: "+ u.to_s + " in "+ temp_update_batch.inspect
+				temp_move_backward.push(temp_update_batch.pop())
+			end
+			temp_update_batch.push(u)
+			temp_update_batch+=temp_move_backward.reverse			 
+		end 
+		#puts "Batch Valid"
+		#puts temp_update_batch.inspect + ":" + temp_update_batch[temp_update_batch.length/2].to_s
+		#temp_update_batch.each {|e| puts e.to_s+": "+ rules[e].inspect}
+		if incorrect then
+			result += temp_update_batch[temp_update_batch.length/2]
+		end
+	end
+	return result
 end
 
 def main()
@@ -67,7 +101,7 @@ def main()
 	rules, updates = prepare(args[0].to_s) 
 
 	puts("Part 1: "+part1(rules, updates).to_s)
-	puts("Part 2: "+part2().to_s)
+	puts("Part 2: "+part2(rules, updates).to_s)
 end
 
 main()
