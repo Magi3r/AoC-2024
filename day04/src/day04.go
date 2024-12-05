d7ee5cd9e9725980714ccae38f0534497398f065
Magi3r <jvd@outlook.de>
2024-12-05 14:45:14 +0100

    publishs day 4

--- day04/src/day04.go
+++ day04/src/day04.go
@@ -1,9 +1,9 @@
 package main
 
 import (
-	"log"
-	"fmt"
 	"bufio"
+	"fmt"
+	"log"
 	"os"
 )
 
@@ -12,20 +12,20 @@ func prepare() []string {
 
 	file, err := os.Open(path)
 	if err != nil {
-        log.Fatal(err)
-    }
-    defer file.Close()
+		log.Fatal(err)
+	}
+	defer file.Close()
 
-    scanner := bufio.NewScanner(file)
-    input := []string {}
-    for scanner.Scan() {
-	    input = append(input, scanner.Text())
-    }
+	scanner := bufio.NewScanner(file)
+	input := []string{}
+	for scanner.Scan() {
+		input = append(input, scanner.Text())
+	}
 
-    if err := scanner.Err(); err != nil {
-        log.Fatal(err)
-    }
-    return input
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+	return input
 
 }
 
@@ -37,11 +37,12 @@ func main() {
 
 func part1(input []string) int {
 	result := 0
-	for y_i,y := range input {
-		for x_i,x := range y {
+	for y_i, y := range input {
+		for x_i, x := range y {
 			if x == 'X' {
 				result += find_valid_1(input, x_i, y_i)
-				log.Print(result)			}
+				log.Print(result)
+			}
 		}
 
 	}
@@ -50,33 +51,33 @@ func part1(input []string) int {
 
 func find_valid_1(input []string, x int, y int) int {
 	result := 0
-	move_directions := [3]int {-1,0,1}
+	move_directions := [3]int{-1, 0, 1}
 	for _, x_move := range move_directions {
 		for _, y_move := range move_directions {
 			if is_valid_1(input, 'M', x, y, x_move, y_move) {
-				result +=  1
+				result += 1
 			}
 		}
 	}
 	return result
 }
 
-func is_valid_1 ( input []string, letter byte, x int, y int, x_move int, y_move int) (bool) {
-	x +=x_move
-	if x<0 || x>= len(input[0]) {
+func is_valid_1(input []string, letter byte, x int, y int, x_move int, y_move int) bool {
+	x += x_move
+	if x < 0 || x >= len(input[0]) {
 		return false
 	}
 	y += y_move
-	if y<0 ||y>=len(input){
+	if y < 0 || y >= len(input) {
 		return false
 	}
 	if input[y][x] == letter {
-		if letter == 'M'{
-			return is_valid_1(input, 'A', x,y,x_move, y_move)
+		if letter == 'M' {
+			return is_valid_1(input, 'A', x, y, x_move, y_move)
 		} else if letter == 'A' {
-			return is_valid_1(input, 'S', x,y,x_move, y_move)
+			return is_valid_1(input, 'S', x, y, x_move, y_move)
 		} else {
-log.Print("Found valid. x: ", x, "y: ",y, "x_move: ", x_move, "y_move: ", y_move)
+			log.Print("Found valid. x: ", x, "y: ", y, "x_move: ", x_move, "y_move: ", y_move)
 			return true
 		}
 	}
@@ -84,36 +85,63 @@ log.Print("Found valid. x: ", x, "y: ",y, "x_move: ", x_move, "y_move: ", y_move
 
 }
 
+type Pos = [2]int
 
 func is_valid_2(input []string, x int, y int) bool {
-	list := make(map[byte][]int)
-	for x_move := range {-1,1} {
-		x_pos:= x+x_move
-		if (x_pos <0 or x_pos >= len(input[0]) {
+	list := map[byte][]Pos{}
+	letter := byte('-')
+	for _, x_move := range [2]int{-1, 1} {
+		x_pos := x + x_move
+		if (x_pos < 0) || (x_pos >= len(input[0])) {
+			log.Print("x pos invalid")
+
 			return false
 		}
-		for y_move := range {-1,1} {
-			y_pos := y+y_move
-			if y_pos <0 or y_pos >= len(input) { return false}
+		for _, y_move := range [2]int{-1, 1} {
+			y_pos := y + y_move
+			if y_pos < 0 || y_pos >= len(input) {
+				log.Print("y pos invalid")
 
-			if input[y_pos][x_pos] == 'M' or input[y_pos][x_pos] =='S' {
-				append(list[x_pos],[y_pos])
+				return false
+			}
+			letter = input[y_pos][x_pos]
+			log.Printf("Checking %d:%d -> %c", y_pos+1, x_pos+1, letter)
+			if letter == 'M' || letter == 'S' {
+				list[letter] = append(list[letter], Pos{x_pos, y_pos})
 			} else {
+				log.Printf("%c: no M or S char", letter)
 				return false
 			}
 		}
 	}
 	if len(list['M']) != 2 {
+		log.Print("too little M")
 		return false
 	}
-	if (list['M'][0][0] == list['M'][1][0]) or (list['M'][0][1]==list['M'][1][1]){
+	if (list['M'][0][0] == list['M'][1][0]) || (list['M'][0][1] == list['M'][1][1]) {
 		return true
 	}
+	log.Print("Not X-shaped")
 	return false
-	
+
 }
 
-
 func part2(input []string) int {
-	return 0
+	result := 0
+	for y_i, y := range input {
+		for x_i, x := range y {
+			if x == 'A' {
+				log.Print("'A' found")
+				if is_valid_2(input, x_i, y_i) {
+					result += 1
+					log.Print(result)
+					// } else {
+
+					// 	log.Printf("%c.%c\n.%c.\n%c.%c", input[y_i-1][x_i-1], input[y_i-1][x_i+1], input[y_i][x_i], input[y_i+1][x_i-1], input[y_i+1][x_i+1])
+				}
+			}
+		}
+
+	}
+	return result
 }
